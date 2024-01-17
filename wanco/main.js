6ca5062f8ea6476eaec725a253080cd18ea25880
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-01-18 00:06:24 +0900

    feat: マウス操作追加

--- wanco/main.js
+++ wanco/main.js
@@ -4,7 +4,9 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 import * as THREE from "three";
-console.log(THREE); // Three.jsのモジュールが正常に読み込まれている
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+console.log(THREE);
 
 
 // シーン：3Dオブジェクトや光源などの要素を含む仮想的な空間を表す。カメラで撮影するためのステージのようなもの。
@@ -12,7 +14,7 @@ const scene = new THREE.Scene();
 
 // カメラ：シーンのどの部分を表示するかを定義
 // PerspectiveCamera(視野角, アスペクト比, 開始距離, 終了距離)
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // カメラ位置の設定
 camera.position.set(0, 0, 500);
@@ -38,27 +40,77 @@ const ballMaterial = new THREE.MeshPhysicalMaterial();
 const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
 scene.add(ballMesh);
 
+// 平行光源の追加
+const directionalLight = new THREE.DirectionalLight(0xfffae8, 3);
+
+// 光の当たる位置調整
+directionalLight.position.set(0.4, 0.5, 1); 
+
+// シーンに追加
+scene.add(directionalLight);
+
+// ポイント光源を追加
+const pointLight = new THREE.PointLight(0xfffae8, 1);
+pointLight.position.set(-200, -200, -200);
+scene.add(pointLight);
+
+// マウス操作
+const Controls = new OrbitControls(camera, renderer.domElement);
+
 // レンダリング関数
 renderer.render(scene, camera);
+
+// ポイント光源のアニメーション
+function animate() {
+  // ポジションを動的に指定
+  pointLight.position.set(
+    200 * Math.sin(Date.now() / 500),
+    200 * Math.sin(Date.now() / 1000),
+    200 * Math.cos(Date.now() / 500)
+  );
+  // フレーム単位で関数実行
+  requestAnimationFrame(animate);
+
+  // レンダリング関数
+  renderer.render(scene, camera);
+}
+
+ // ブラウザリサイズ対応
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
+
+window.addEventListener("resize", onWindowResize);
+animate(); // アニメーション関数の実行
 
 
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
+  <ul class="side-list">
+    <li>
+      <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      </a>
+    </li>
+    <li>
+      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
+      </a>
+    </li>
+    <li><h1>three.js</h1></li>
+    <li>
     <div class="card">
       <button id="counter" type="button"></button>
-    </div>
+      </div>
+    </li>
+    <li>
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
-  </div>
+    </li>
+  </ul>
 `
 
 setupCounter(document.querySelector('#counter'))
